4bb3418e6e92bee171c9fe18f84e4ce7d9144336
Jesper Høgsted <81517227+JesperHogstedDk@users.noreply.github.com>
2023-09-14 20:07:15 +0200

    read

---
+++ client/src/RenderData.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './render-data.css'
+
+const RenderData = ({ data }) => {
+    return (
+        <div
+            className='render-data'
+        >
+            <pre>
+                {
+                    JSON.stringify(data, null, 3)
+                }
+            </pre>
+        </div>
+    );
+};
+
+export default RenderData;
+
